091e2016fa1fbad9e6038c80fa42e477f8a6cf39
GitNotFound404 <tazimra@gmail.com>
2025-09-11 07:09:57 +0530

    Wiki gone, our sentence implemented.

--- scripts/index.js
+++ scripts/index.js
@@ -1,186 +1,102 @@
-const topicElement = document.getElementById("topic");
 const sentenceElement = document.getElementById("sentence");
 const scrambledSentenceElement = document.getElementById("scambled-sentence");
 const userGuessInputElement = document.getElementById("user-guess");
 
-const fetchAddr = "https://gnftest001-backend.onrender.com/api/get-sentences-on-random-topic";
-const fetchAddrTest = "http://127.0.0.1:10000/api/get-sentences-on-random-topic";
+const fetchAddr = "https://gnftest001-backend.onrender.com/toc128_api/get_sentences_list";
+const fetchAddrTest = "http://127.0.0.1:10000/toc128_api/get_sentences_list/";
+
+let addrFetch = fetchAddrTest; // Change this according to build.
 
 let scrambled_sentence = [];
-
-let addrFetch = fetchAddr; // Change this according to build.
+let sentences = [];
+let wait_done = false;
+let sentenceVisible = true;
 
 function chooseRandom(array) {
     return array[Math.floor(Math.random() * array.length)];
 }
 
-let storedSentences = [];
-
-const maxMemory = 3;
-const maxStorage = 3;
-let fetchBusy = false;
-
-function newSet(topic, sentences) {
-    return {topic: topic, sentences: sentences, memoryLeft: Math.min(sentences.length, maxMemory)};
+async function fetchSentences() {
+    sentences = await (await fetch(addrFetch)).json();
 }
 
-function removeUsedSet() {
-    for (let i = storedSentences.length-1; i >= 0; i--) {
-        console.log(`remove: ${JSON.stringify(storedSentences)}`);
-        if (storedSentences[i].memoryLeft <= 0) {
-            storedSentences.splice(i, 1);
-        }
-        console.log(`removed?: ${JSON.stringify(storedSentences)}`);
-    }
-}
-
-async function fetchSetAndStore() {
-    console.log(`Fetch Busy: ${fetchBusy}`);
-
-    if ((storedSentences.length < maxStorage) && !fetchBusy) {
-        fetchBusy = true;
-        let set = await fetchSet();
-        fetchBusy = false;
-
-        storedSentences.push(set);
-        return getUsableSet();
-    }
-}
-
-async function storeFetchedSet() {
-    console.log(`Fetch Busy: ${fetchBusy}`);
-
-    if ((storedSentences.length < maxStorage) && !fetchBusy) {
-        fetchBusy = true;
-        let set = await fetchSet();
-        fetchBusy = false;
-
-        storedSentences.push(set);
-    }
-}
-
-function waitForUsableSet() {
+function waitForArray(array) {
     return new Promise(resolve => {
         const intervalId = setInterval(() => {
-            const usableSet = checkUsableSet();
-            if (usableSet) {
+            if (array.length>0) {
                 clearInterval(intervalId);
-                resolve(usableSet);
+                resolve(array);
             }
         }, 250);
     });
 }
 
-let waitedForUsableSet = false;
-async function getUsableTopicSentence() {
-    let set = getUsableSet();
-
-    // If there is no usable set, and a fetch is busy, wait for it.
-    if (!set) {
-        if (fetchBusy) {
-            set = await waitForUsableSet();
-        } else {
-            // Otherwise, fetch a new set (and store it).
-            set = await fetchSetAndStore();
-        }
+async function getRandomSentence() {
+    if (!wait_done) {
+        await waitForArray(sentences);
+        wait_done = true;
     }
 
-
-    console.log(`Usable Set: ${JSON.stringify(set.sentences)}`);
-    console.log(`got set: ${JSON.stringify(set)}`);
-
-    return [set.topic, chooseRandom(set.sentences)];
-}
-
-function getUsableSet() {
-    for (let i = 0; i < storedSentences.length; i++) {
-        if (storedSentences[i].memoryLeft > 0) {
-            storedSentences[i].memoryLeft--;
-            return storedSentences[i];
-        }
-    }
-}
-
-function checkUsableSet() {
-    for (let i = 0; i < storedSentences.length; i++) {
-        if (storedSentences[i].memoryLeft > 0) {
-            return storedSentences[i];
-        }
-    }
-}
-
-async function fetchSet() {
-    let response = await fetch(addrFetch);
-    let jsonResponse = await response.json();
-    return newSet(jsonResponse[2], jsonResponse[0]);
+    return chooseRandom(sentences);
 }
 
 async function showSentence() {
-    sentenceElement.style.opacity = 0;
+    //toggleOriginalSentenceVisibility();
+    sentenceVisible ? toggleOriginalSentenceVisibility() : null;
     userGuessInputElement.value = "";
-    topicElement.innerText = "Loading...";
     sentenceElement.innerText = "Loading...";
     scrambledSentenceElement.innerText = "Loading...";
 
-    let [topic, sentence] = await getUsableTopicSentence(); // Get usable sentence (from storage or fetch)
-    console.log(`Topic: ${topic}\nSentence: ${sentence}.`);
-    removeUsedSet(); // Remove used set, if a set becomes used (memory left: 0)
+    let sentence = await getRandomSentence();
     
-    topicElement.innerText = `Topic: ${topic}\n`
     sentenceElement.innerText = `Sentence: ${sentence}${
         !(sentence.slice(-1).includes('.') || 
         sentence.slice(-1).includes('?') || 
         sentence.slice(-1).includes('!')) ? '.' : ''}`; // Display the topic & sentence`
     
     scrambled_sentence = scrambleSentence(sentence);
-    scrambledSentenceElement.innerText = `Scramabled Sentence: ${scrambled_sentence}`;
-
-    await storeFetchedSet(); // Store new set of sentences for later (if not maxed out)
+    scrambledSentenceElement.innerText = `Scramabled Sentence: ${scrambled_sentence.split(' ').length <= 5 ? scrambled_sentence.toLowerCase() : scrambled_sentence}`;
 }
 
 function scrambleSentence(sentence) {
+    sentence = sentence.substring(0, sentence.length-1);
+
     const words = sentence.split(' ');
     const wordsLength = words.length;
 
-    let scrambled_sentence = [];
+    let scrambled_sentence = sentence;
 
-    for (let _ = 0; _ < wordsLength; _++) {
-        const index = Math.floor(Math.random()*words.length);
-        console.log(index);
+    while (sentence.toLowerCase() === scrambled_sentence.toLowerCase()) {
+        scrambled_sentence = [];
 
-        scrambled_sentence.push(words[index]);
-        words.splice(index, 1);   
+        for (let _ = 0; _ < wordsLength; _++) {
+            const index = Math.floor(Math.random()*words.length);
+
+            scrambled_sentence.push(words[index]);
+            words.splice(index, 1);
+        }
+
+        scrambled_sentence = scrambled_sentence.join(' ');
     }
-    
 
-    return scrambled_sentence.join(' ');
+    return scrambled_sentence;
+}
+
+function toggleOriginalSentenceVisibility() {
+    sentenceElement.style.opacity = sentenceVisible ? 0 : 1;
+    sentenceVisible = !sentenceVisible;
 }
 
 function checkGuess(event) {
     if (event.key === 'Enter') {
-        console.log('Pressed Enter')
-        sentenceElement.style.opacity = 1;
+        if (!sentenceVisible) {
+            toggleOriginalSentenceVisibility();
+        } else {
+            showSentence();
+        }
     }
 }
 
 userGuessInputElement.addEventListener('keydown', checkGuess);
 
-/*
-function wordsUsed(scrambled_sentence) {
-    words = scrambled_sentence.split(' ');
-    user_word = userGuessInputElement.value.split(' ');
-
-    for (i in (user_word.length < words.length ? user_word : words)) {
-        used_words = 0
-
-        if (user_word[i] === words[i]) {
-            used_words++;
-            console.log(used_words)
-        }
-    }
-}
-
-while (topicElement.innerText !== "Loading..." || topicElement.innerText !== "Click Get Sentence" || sentenceElement.innerText !== "Loading..." || sentenceElement.innerText !== "Click Get Sentence") {
-    wordsUsed();
-}
-*/
+fetchSentences();
---
+++ scripts/old_index2.js
@@ -0,0 +1,186 @@
+const topicElement = document.getElementById("topic");
+const sentenceElement = document.getElementById("sentence");
+const scrambledSentenceElement = document.getElementById("scambled-sentence");
+const userGuessInputElement = document.getElementById("user-guess");
+
+const fetchAddr = "https://gnftest001-backend.onrender.com/api/get-sentences-on-random-topic";
+const fetchAddrTest = "http://127.0.0.1:10000/api/get-sentences-on-random-topic";
+
+let scrambled_sentence = [];
+// use predefined more
+let addrFetch = fetchAddrTest; // Change this according to build.
+
+function chooseRandom(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+let storedSentences = [];
+
+const maxMemory = 3;
+const maxStorage = 3;
+let fetchBusy = false;
+
+function newSet(topic, sentences) {
+    return {topic: topic, sentences: sentences, memoryLeft: Math.min(sentences.length, maxMemory)};
+}
+
+function removeUsedSet() {
+    for (let i = storedSentences.length-1; i >= 0; i--) {
+        console.log(`remove: ${JSON.stringify(storedSentences)}`);
+        if (storedSentences[i].memoryLeft <= 0) {
+            storedSentences.splice(i, 1);
+        }
+        console.log(`removed?: ${JSON.stringify(storedSentences)}`);
+    }
+}
+
+async function fetchSetAndStore() {
+    console.log(`Fetch Busy: ${fetchBusy}`);
+
+    if ((storedSentences.length < maxStorage) && !fetchBusy) {
+        fetchBusy = true;
+        let set = await fetchSet();
+        fetchBusy = false;
+
+        storedSentences.push(set);
+        return getUsableSet();
+    }
+}
+
+async function storeFetchedSet() {
+    console.log(`Fetch Busy: ${fetchBusy}`);
+
+    if ((storedSentences.length < maxStorage) && !fetchBusy) {
+        fetchBusy = true;
+        let set = await fetchSet();
+        fetchBusy = false;
+
+        storedSentences.push(set);
+    }
+}
+
+function waitForUsableSet() {
+    return new Promise(resolve => {
+        const intervalId = setInterval(() => {
+            const usableSet = checkUsableSet();
+            if (usableSet) {
+                clearInterval(intervalId);
+                resolve(usableSet);
+            }
+        }, 250);
+    });
+}
+
+let waitedForUsableSet = false;
+async function getUsableTopicSentence() {
+    let set = getUsableSet();
+
+    // If there is no usable set, and a fetch is busy, wait for it.
+    if (!set) {
+        if (fetchBusy) {
+            set = await waitForUsableSet();
+        } else {
+            // Otherwise, fetch a new set (and store it).
+            set = await fetchSetAndStore();
+        }
+    }
+
+
+    console.log(`Usable Set: ${JSON.stringify(set.sentences)}`);
+    console.log(`got set: ${JSON.stringify(set)}`);
+
+    return [set.topic, chooseRandom(set.sentences)];
+}
+
+function getUsableSet() {
+    for (let i = 0; i < storedSentences.length; i++) {
+        if (storedSentences[i].memoryLeft > 0) {
+            storedSentences[i].memoryLeft--;
+            return storedSentences[i];
+        }
+    }
+}
+
+function checkUsableSet() {
+    for (let i = 0; i < storedSentences.length; i++) {
+        if (storedSentences[i].memoryLeft > 0) {
+            return storedSentences[i];
+        }
+    }
+}
+
+async function fetchSet() {
+    let response = await fetch(addrFetch);
+    let jsonResponse = await response.json();
+    return newSet(jsonResponse[2], jsonResponse[0]);
+}
+
+async function showSentence() {
+    sentenceElement.style.opacity = 0;
+    userGuessInputElement.value = "";
+    topicElement.innerText = "Loading...";
+    sentenceElement.innerText = "Loading...";
+    scrambledSentenceElement.innerText = "Loading...";
+
+    let [topic, sentence] = await getUsableTopicSentence(); // Get usable sentence (from storage or fetch)
+    console.log(`Topic: ${topic}\nSentence: ${sentence}.`);
+    removeUsedSet(); // Remove used set, if a set becomes used (memory left: 0)
+    
+    topicElement.innerText = `Topic: ${topic}\n`
+    sentenceElement.innerText = `Sentence: ${sentence}${
+        !(sentence.slice(-1).includes('.') || 
+        sentence.slice(-1).includes('?') || 
+        sentence.slice(-1).includes('!')) ? '.' : ''}`; // Display the topic & sentence`
+    
+    scrambled_sentence = scrambleSentence(sentence);
+    scrambledSentenceElement.innerText = `Scramabled Sentence: ${scrambled_sentence}`;
+
+    await storeFetchedSet(); // Store new set of sentences for later (if not maxed out)
+}
+
+function scrambleSentence(sentence) {
+    const words = sentence.split(' ');
+    const wordsLength = words.length;
+
+    let scrambled_sentence = [];
+
+    for (let _ = 0; _ < wordsLength; _++) {
+        const index = Math.floor(Math.random()*words.length);
+        console.log(index);
+
+        scrambled_sentence.push(words[index]);
+        words.splice(index, 1);   
+    }
+    
+
+    return scrambled_sentence.join(' ');
+}
+
+function checkGuess(event) {
+    if (event.key === 'Enter') {
+        console.log('Pressed Enter')
+        sentenceElement.style.opacity = 1;
+    }
+}
+
+userGuessInputElement.addEventListener('keydown', checkGuess);
+
+/*
+function wordsUsed(scrambled_sentence) {
+    words = scrambled_sentence.split(' ');
+    user_word = userGuessInputElement.value.split(' ');
+
+    for (i in (user_word.length < words.length ? user_word : words)) {
+        used_words = 0
+
+        if (user_word[i] === words[i]) {
+            used_words++;
+            console.log(used_words)
+        }
+    }
+}
+
+while (topicElement.innerText !== "Loading..." || topicElement.innerText !== "Click Get Sentence" || sentenceElement.innerText !== "Loading..." || sentenceElement.innerText !== "Click Get Sentence") {
+    wordsUsed();
+}
+*/
